2c0354c15c2d158c7f80de102d903bb454de6f45
Sharon <seaustin@live.unc.edu>
2014-07-02 09:50:35 -0400

    first commit, finished for now

---
+++ sorter.js
@@ -0,0 +1,253 @@
+$(document).ready(function() {
+
+$( ".php-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.php)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.php)').hide("fade", function() { });
+		$('.php-button').css("background-color", "#99CCFF");
+
+		$('.javascript-button').css("background-color", "#CCCCFF");
+		$('.wordpress-button').css("background-color", "#CCCCFF");
+		$('.omeka-button').css("background-color", "#CCCCFF");
+
+	}
+
+	});
+
+$( ".javascript-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.javascript)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.javascript)').hide("fade", function() { });
+		$('.javascript-button').css("background-color", "#99CCFF");
+
+		$('.php-button').css("background-color", "#CCCCFF");
+		$('.wordpress-button').css("background-color", "#CCCCFF");
+		$('.omeka-button').css("background-color", "#CCCCFF");		
+
+	}
+
+	});
+
+$( ".wordpress-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.wordpress)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.wordpress)').hide("fade", function() { });
+		$('.wordpress-button').css("background-color", "#99CCFF");
+
+		$('.javascript-button').css("background-color", "#CCCCFF");
+		$('.php-button').css("background-color", "#CCCCFF");
+		$('.omeka-button').css("background-color", "#CCCCFF");
+	}
+
+	});
+
+$( ".omeka-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.omeka)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.omeka)').hide("fade", function() { });
+		$('.omeka-button').css("background-color", "#99CCFF");
+
+		$('.javascript-button').css("background-color", "#CCCCFF");
+		$('.wordpress-button').css("background-color", "#CCCCFF");
+		$('.php-button').css("background-color", "#CCCCFF");
+	}
+
+	});
+
+$( ".omeka-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.omeka)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.omeka)').hide("fade", function() { });
+		$('.omeka-button').css("background-color", "#99CCFF");
+
+		$('.javascript-button').css("background-color", "#CCCCFF");
+		$('.wordpress-button').css("background-color", "#CCCCFF");
+		$('.php-button').css("background-color", "#CCCCFF");
+	}
+
+	});
+
+	$( ".testing-button" ).click(function() {
+	  
+
+		if ($("ul.portfolio-list > li")){
+
+			$('ul.portfolio-list > li:hidden(.testing)').show("fade", function() { });
+			$('ul.portfolio-list > li:not(.testing)').hide("fade", function() { });
+			$('.testing-button').css("background-color", "#99CCFF");
+
+			$('.wireframing-button').css("background-color", "#CCCCFF");
+			$('.cards-button').css("background-color", "#CCCCFF");
+			$('.ia-button').css("background-color", "#CCCCFF");
+			$('.persona-button').css("background-color", "#CCCCFF");
+			$('.feedback-button').css("background-color", "#CCCCFF");
+			$('.task-button').css("background-color", "#CCCCFF");
+
+		}
+
+	});
+
+	$( ".wireframing-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.wireframing)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.wireframing)').hide("fade", function() { });
+		$('.wireframing-button').css("background-color", "#99CCFF");
+
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+	$( ".cards-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.cards)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.cards)').hide("fade", function() { });
+		$('.cards-button').css("background-color", "#99CCFF");
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+	$( ".ia-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.ia)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.ia)').hide("fade", function() { });
+		$('.ia-button').css("background-color", "#99CCFF");
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+	$( ".persona-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.persona)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.persona)').hide("fade", function() { });
+		$('.persona-button').css("background-color", "#99CCFF");
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+	$( ".feedback-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.feedback)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.feedback)').hide("fade", function() { });
+		$('.feedback-button').css("background-color", "#99CCFF");
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+$( ".task-button" ).click(function() {
+  
+
+	if ($("ul.portfolio-list > li")){
+
+		$('ul.portfolio-list > li:hidden(.task)').show("fade", function() { });
+		$('ul.portfolio-list > li:not(.task)').hide("fade", function() { });
+		$('.task-button').css("background-color", "#99CCFF");
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+
+	}
+
+});
+
+
+
+});
+
+//wireframing cards ia persona feedback task testing
+
+
+
+$( ".clear-filters").click(function(){
+
+	$('ul.portfolio-list > li').show("fade", function(){ });
+
+		$('.javascript-button').css("background-color", "#CCCCFF");
+		$('.wordpress-button').css("background-color", "#CCCCFF");
+		$('.php-button').css("background-color", "#CCCCFF");
+		$('.omeka-button').css("background-color", "#CCCCFF");
+
+
+		$('.wireframing-button').css("background-color", "#CCCCFF");
+		$('.cards-button').css("background-color", "#CCCCFF");
+		$('.ia-button').css("background-color", "#CCCCFF");
+		$('.persona-button').css("background-color", "#CCCCFF");
+		$('.feedback-button').css("background-color", "#CCCCFF");
+		$('.testing-button').css("background-color", "#CCCCFF");
+		$('.task-button').css("background-color", "#CCCCFF");
+
+
+
+});
